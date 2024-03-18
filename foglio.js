8ed547dabfac64860e05b7aff0fdd2422325fbab
agostino <agostinocuomo9@gmail.com>
2024-03-18 14:20:10 +0100

    esercizio js

---
+++ foglio.js
@@ -0,0 +1,57 @@
+/* TRACCIA 1
+ */ 
+
+let bottone1 = document.querySelector('.bottone1');
+let bottone2 = document.querySelector('#bottone2');
+let bottone3 = document.querySelector('.bottone3');
+let paragrafo1 = document.querySelector('.paragrafo1');
+let paragrafo2 = document.querySelector('.paragrafo2');
+let paragrafo3 = document.querySelector('.paragrafo3');
+let paragrafo = document.querySelectorAll('p');
+
+bottone1.addEventListener('click', ()=>{
+
+ paragrafo.forEach(query => {
+    let r = Math.floor(Math.random() * (256 - 0));
+    let g = Math.floor(Math.random() * (256 - 0));
+    let b = Math.floor(Math.random() * (256 - 0));
+    query.style.color= `rgb(${r},${g},${b})`;
+ });
+} );
+
+
+bottone2.addEventListener('click', 
+function Grassetto() {
+    paragrafo.forEach(query=>
+    query.classList.toggle('bold')
+    );
+}
+);
+
+bottone3.addEventListener('click', 
+function Grassetto() {
+    paragrafo.forEach(query=>
+    query.classList.toggle('visibile')
+    );
+}
+);
+
+
+/* TRACCIA 2 */
+
+let titolo= document.querySelector('#titolo');
+let descrizione = document.querySelector('#descrizione');
+let CreaArticolo = document.querySelector('.CreaArticolo');
+let contenitore= document.querySelector('.contenitore');
+
+CreaArticolo.addEventListener( 'click',()=>{
+    let r = Math.floor(Math.random() * (256 - 0));
+    let g = Math.floor(Math.random() * (256 - 0));
+    let b = Math.floor(Math.random() * (256 - 0));
+ let box = document.createElement('div');
+ box.style.backgroundColor= `rgb(${r},${g},${b})`;
+ box.classList.add("box","m-5");
+ box.innerHTML= `<h2> ${titolo.value} <h2> <br> <p> ${descrizione.value} `
+ contenitore.appendChild(box);
+ 
+})
